b3965b997351d9a87535b96a70bed3301e5e88a8
gastank <42421688+gastank@users.noreply.github.com>
2025-07-23 12:46:07 -0700

    [Balance] hard disable dungeon slice

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -572,6 +572,7 @@ struct druid_t final : public parse_player_effects_t
     double initial_astral_power = 0.0;
     int initial_moon_stage = static_cast<int>( moon_stage_e::NEW_MOON );
     int initial_orbit_breaker_stacks = -1;
+    bool enable_dungeon_slice = false;
 
     // Feral
     double adaptive_swarm_jump_distance_melee = 5.0;
@@ -12678,6 +12679,12 @@ bool druid_t::validate_fight_style( fight_style_e style ) const
   switch ( specialization() )
   {
     case DRUID_BALANCE:
+      if ( style == FIGHT_STYLE_DUNGEON_SLICE && !options.enable_dungeon_slice )
+      {
+        sim->error( "DungeonSlice is disabled for Balance Druids. To force enable, use druid.enable_dungeon_slice=1 option." );
+        sim->cancel();
+      }
+
       if ( style != FIGHT_STYLE_PATCHWERK && style != FIGHT_STYLE_DUNGEON_ROUTE )
         return false;
 
@@ -14040,6 +14047,7 @@ void druid_t::create_options()
   add_option( opt_float( "druid.initial_astral_power", options.initial_astral_power ) );
   add_option( opt_int( "druid.initial_moon_stage", options.initial_moon_stage ) );
   add_option( opt_int( "druid.initial_orbit_breaker_stacks", options.initial_orbit_breaker_stacks ) );
+  add_option( opt_bool( "druid.enable_dungeon_slice", options.enable_dungeon_slice ) );
 
   // Feral
   add_option( opt_float( "druid.adaptive_swarm_jump_distance_melee", options.adaptive_swarm_jump_distance_melee ) );
